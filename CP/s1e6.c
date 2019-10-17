5b6f69aa7d31a6b009c2eef88fd2e416853f4f3d
hehaoran <58555249@qq.com>
2019-10-17 21:27:52 +0800

    add s1e6

--- CP/s1e6.c
+++ CP/s1e6.c
@@ -1,2 +1,39 @@
 #include <stdio.h>
+#include<math.h>
 
+#define MAIZI 25000
+
+int func1()
+{
+    int result;
+
+    result = pow(1,2) + pow(2,3) + pow(3,4) + pow(4,5) + pow(5,6);
+    printf("值为:%d\n",result);
+
+    return 0;
+}
+
+int func2()
+{
+    int i;
+    unsigned long long int sum = 0;
+    unsigned long long int temp;
+
+    for (i=0;i<64;i++)
+    {
+        temp = pow(2,i);
+        sum += temp; 
+    }
+
+    printf("舍罕王应该给予达依尔%llu粒麦子!\n如果每25000粒麦子为1kg,那么应该给%llukg麦子\n",sum,sum / MAIZI);
+
+    return 0;
+}
+
+int main()
+{
+    // func1();
+    func2();
+    
+    return 0;
+}
