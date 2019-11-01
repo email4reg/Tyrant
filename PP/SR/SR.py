d66eed505082362e9dc05b389fb61b87a86c6802
hehaoran <58555249@qq.com>
2019-11-01 10:29:24 +0800

    add SI, SIS, SIR model

--- PP/SR/SR.py
+++ PP/SR/SR.py
@@ -1,53 +1,11 @@
 # python 3.7.4 @hehaoran
-import pylab as pl
-import scipy.integrate as spi
 import numpy as np
 import tensorly as tl
 from tensorly.decomposition import parafac
 
 
-# 张量分解
-# x = tl.tensor(np.arange(20).reshape(3,4,2).astype(float))
-# core,factors = parafac(x,rank = 3)
-# print(factors)
+# Tensor Decomposition
+x = tl.tensor(np.arange(20).reshape(3,4,2).astype(float))
+core,factors = parafac(x,rank = 3)
+print(factors)
 
-# SI模型
-beta = 1.4247
-"""the likelihood that the disease will be transmitted from an infected to a susceptible
-individual in a unit time is β"""
-gamma = 0
-#gamma is the recovery rate and in SI model, gamma equals zero
-I0 = 1e-6
-#I0 is the initial fraction of infected individuals
-ND = 70
-#ND is the total time step
-TS = 1.0
-INPUT = (1.0-I0, I0)
-
-
-def diff_eqs(INP, t):
-    '''The main set of equations'''
-    Y = np.zeros((2))
-    V = INP
-    Y[0] = - beta * V[0] * V[1] + gamma * V[1]
-    Y[1] = beta * V[0] * V[1] - gamma * V[1]
-    return Y   # For odeint
-
-
-t_start = 0.0
-t_end = ND
-t_inc = TS
-t_range = np.arange(t_start, t_end+t_inc, t_inc)
-RES = spi.odeint(diff_eqs, INPUT, t_range)
-"""RES is the result of fraction of susceptibles and infectious individuals at each time step respectively"""
-print(RES)
-
-#Ploting
-pl.plot(RES[:, 0], '-bs', label='Susceptibles')
-pl.plot(RES[:, 1], '-ro', label='Infectious')
-pl.legend(loc=0)
-pl.title('SI epidemic without births or deaths')
-pl.xlabel('Time')
-pl.ylabel('Susceptibles and Infectious')
-pl.savefig('2.5-SI-high.png', dpi=900)  # This does increase the resolution.
-pl.show()
