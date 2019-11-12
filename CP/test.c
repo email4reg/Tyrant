eb06bb6c1a9b2b741681d90f9ac66cd5ce7ab04b
hehaoran <58555249@qq.com>
2019-11-12 15:06:15 +0800

    update test

--- CP/test.c
+++ CP/test.c
@@ -3,8 +3,10 @@
 #include <math.h>
 #include <string.h>
 
+#define N 30 // 最多的学生数
+#define M 10 // 最多的课程数
 
-int main()
+int func1()
 {
     double height = 100;
     double sum = 0;
@@ -18,4 +20,74 @@ int main()
     }
 
     return 0;
+}
+
+void input(float a[N][M], int n, int m)
+{
+    int i, j;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("请输入%d号学生%d门课的成绩: ", i + 1, m);
+
+        for (j = 0; j < m; j++)
+        {
+            scanf("%f", &a[i][j]);
+        }
+    }
+}
+
+void stu_mean(float a[N][M], int n, int m)
+{
+    int i, j;
+    float sum;
+
+    for (i = 0; i < n; i++)
+    {
+        sum = 0;
+        for (j = 0; j < m; j++)
+        {
+            sum += a[i][j];
+        }
+        printf("%d号学生的成绩为:", i+1);
+
+        for (j = 0; j < m; j++)
+        {
+            printf("%6.2f", a[i][j]);
+        }
+        printf("平均分为: %6.2f\n", sum/m);
+    }
+}
+
+void cour_mean(float a[N][M], int n, int m)
+{
+    int i, j;
+    float sum;
+
+    for (i = 0; i < m; i++)
+    {
+        sum = 0;
+
+        for (j = 0; j < n; j++)
+        {
+            sum += a[j][i];
+        }
+        printf("第%d门课的平均成绩为: %6.2f\n", i+1, sum/n);
+    }
+}
+
+int main()
+{
+    int m, n;
+    float a[N][M];
+
+    printf("输入学生数n, 和课程数m: ");
+    scanf("%d %d", &n, &m);
+
+    input(a, n, m);
+    stu_mean(a, n, m);
+    cour_mean(a, n, m);
+
+    return 0;
+
 }
