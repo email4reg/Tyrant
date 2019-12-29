cb90c0a487d69141207e77dcc965c5acc91c47aa
hehaoran <58555249@qq.com>
2019-12-29 15:14:08 +0800

    add #s1e21

--- CP/s1e21.c
+++ CP/s1e21.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stdbool.h>
 
-void fun1()
+void func1()
+{
+    char a = 'F';
+    int f = 123;
+
+    char *pa = &a;
+    int *pb = &f;
+
+    printf("a = %c\n", *pa);
+    printf("f = %d\n", *pb);
+
+    *pa = 'C';
+    *pb += 1;
+
+    printf("a = %c\n", *pa);
+    printf("f = %d\n", *pb);
+
+    printf("sizeof pa = %lu\n", sizeof(pa));
+    printf("sizeof pb = %lu\n", sizeof(pb));
+
+    printf("the address of a is %p\n", pa);
+    printf("the address of f is %p\n", pb);
+
+}
+
+void func2()
+{
+    int a;
+    int *pa = &a;
+
+    printf("请输入一个整数:");
+    scanf("%d",&a);
+
+    printf("a = %d\n", a);
+
+    printf("请重新输入一个整数:");
+    scanf("%d",pa);
+
+    printf("a = %d\n", *pa);
+
+}
+
+
+void fun3()
 {
     int a, b, c, t;
     int *pa, *pb, *pc;
@@ -36,15 +78,57 @@ void fun1()
     }
 
     printf("%d <= %d <= %d\n", *pa, *pb, *pc);
-    printf("%d <= %d <= %d\n", a, b, c);
+    // printf("%d <= %d <= %d\n", a, b, c);
 
 }
 
+int func4()
+{
+    int i, j, n, cubed, sum = 0;
+
+    printf("请输入一个整数:");
+    scanf("%d",&n);
+
+    cubed = n * n * n;
+
+    for (i = 1; i < cubed; i += 2)
+    {
+        for (j = i; j < cubed; j += 2)
+        {
+            sum += j;
+            if (sum == cubed)
+            {
+                if (j - i > 4)
+                {
+                    printf("%d = %d + %d ... + %d\n", cubed, i, i + 2, j);
+                }
+                else
+                {
+                    printf("%d = %d + %d + %d\n", cubed, i, i + 2, i + 4);
+                }
+                goto FINDIT;
+                
+            }
+
+            if (sum > cubed)
+            {
+                sum = 0;
+                break;
+            }
+        }
+    }
+
+FINDIT:
+    return 0;
+}
 
 int main()
 {
-    func1();
+    // func1();
     // func2();
+    // func3();
+    // func4();
+    func5();
 
     return 0;
 }
